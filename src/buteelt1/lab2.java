e304e4b67d374db2971d64557b4d8645b18e2737
batkhuu <batkhuu@BB>
2024-03-05 09:49:21 +0800

    hiiii

---
+++ src/buteelt1/lab2.java
@@ -0,0 +1,89 @@
+package buteelt1;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class lab2 {
+
+    public static void main(String[] args) {
+        List<Integer> myList = new ArrayList<>(); 
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            System.out.println("1.элемент нэмэх");
+            System.out.println("2.элемент хасах");
+            System.out.println("3.бүх элементийг харах");
+            System.out.println("4.эрэмбэлэх");
+            System.out.println("5.хоосон эсэхийг шалгах ");
+            System.out.println("6.бүх элементний нийлбэр");
+            System.out.println("7.Дуусгах");
+            System.out.println("----------------------------------");
+            int b = sc.nextInt();
+            switch (b) {
+                case 1:
+                    addElement(myList, sc);
+                    break;
+                case 2:
+                    removeElement(myList, sc);
+                    break;
+                case 3:
+                    Element(myList);
+                    break;
+                case 4:
+                    sortElement(myList);
+                    break;
+                case 5:
+                    checkEmpty(myList);
+                    break;
+                case 6:
+                    Sum(myList);
+                    break;
+                case 7:
+                    System.out.println("Программаа хаагдлаа.");
+                    System.exit(0);
+                    break;
+            }
+        }
+    }
+
+    public static void addElement(List<Integer> myList, Scanner sc) {
+        System.out.println("Нэмэх элемент:");
+        int element = sc.nextInt();
+        myList.add(element);
+    }
+
+    public static void removeElement(List<Integer> myList, Scanner sc) {
+        System.out.println("Хасах элэмэнт:");
+        int element = sc.nextInt();
+        if (myList.contains(element)) {
+            myList.remove(Integer.valueOf(element));
+        } else {
+            System.out.println("Олдсонгүй");
+        }
+    }
+
+    public static void Element(List<Integer> myList) {
+        System.out.println("Жагсаалтан дахь бүх элемент:" + myList);
+    }
+
+    public static void sortElement(List<Integer> myList) {
+        Collections.sort(myList); 
+        System.out.println("Эрэмбэлэгдсэн жагсаалт:" + myList);
+    }
+    
+    public static void checkEmpty(List<Integer> myList) {
+        if (myList.isEmpty()) {
+            System.out.println("Жагсаалт хоосон байна.");
+        } else {
+            System.out.println("Жагсаалтанд:" + myList + "Эдгээр элементүүд байна.");
+        }
+    }
+
+    public static void Sum(List<Integer> myList) {
+        int sum = 0;
+        for (int i = 0; i < myList.size(); i++) {
+            sum += myList.get(i);
+        }
+        System.out.println("Нийлбэр:" + sum);
+    }
+}
